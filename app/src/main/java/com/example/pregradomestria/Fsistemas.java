1fb39964607bc83cc747cc44a82703e9116548d8
Unknown <kerlysamaniego@gmail.com>
2020-02-06 20:57:45 -0500

    imagenes

--- app/src/main/java/com/example/pregradomestria/Fsistemas.java
+++ app/src/main/java/com/example/pregradomestria/Fsistemas.java
@@ -2,13 +2,31 @@ package com.example.pregradomestria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Fsistemas extends AppCompatActivity {
+    Button auditoria;
+    String[] urls = new String[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fsistemas);
+
+        auditoria = findViewById(R.id.btnauditoria);
+
+        urls[0] ="http://www.fisc.utp.ac.pa/maestria-y-postgrado-en-auditoria-de-sistemas-y-evaluacion-de-control-informatico";
+
+        auditoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),webfacultades.class);
+                intent.putExtra("links",urls[0]);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pregradomestria/webfacultades.java
+++ app/src/main/java/com/example/pregradomestria/webfacultades.java
@@ -2,13 +2,38 @@ package com.example.pregradomestria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class webfacultades extends AppCompatActivity {
+    WebView wfacultades;
+    @Override
+    public void onBackPressed() {
+        if (wfacultades.canGoBack()){
+            wfacultades.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webfacultades);
+
+        wfacultades=findViewById(R.id.facultadesweb);
+        Intent intent = getIntent();
+
+        String webSite=intent.getStringExtra("links");
+
+        wfacultades.setWebViewClient(new WebViewClient());
+        wfacultades.loadUrl(webSite);
+
+        WebSettings webSettings= wfacultades.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
