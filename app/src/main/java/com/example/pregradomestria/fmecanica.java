567f3c14114e1d706c4cbcf9c292a96ea25c99dc
Unknown <kerlysamaniego@gmail.com>
2020-02-19 02:14:37 -0500

    implementacion de listview

--- app/src/main/java/com/example/pregradomestria/fmecanica.java
+++ app/src/main/java/com/example/pregradomestria/fmecanica.java
@@ -2,9 +2,13 @@ package com.example.pregradomestria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -27,7 +31,38 @@ public class fmecanica extends AppCompatActivity {
         names.add("Maestría en Energías Renovables y Ambiente");
         names.add("Maestría en Ingeniería de Planta");
         names.add("Maestría en Mantenimiento de Plantas");
+
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
         listpostgrados.setAdapter(adapter);
+
+        urls[0] ="http://www.fim.utp.ac.pa/maestria-en-ciencias-de-la-ingenieria-mecanica";
+        urls[1] ="http://www.fii.utp.ac.pa/dual-master-georgia-tech-utp-admission-process";
+        urls[2] ="";
+        urls[3] ="";
+        urls[4] ="";
+        urls[5] ="";
+
+        listpostgrados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(),webfacultades.class);
+                        intent.putExtra("links",urls[0]);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Intent i = new Intent(getApplicationContext(),webfacultades.class);
+                        i.putExtra("links",urls[1]);
+                        startActivity(i);
+                        break;
+
+                }
+            }
+
+        });
     }
+
+
 }
